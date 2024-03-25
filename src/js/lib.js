8960f1410944c8496b0bbcdaec64791c8e312d54
Daniel Fairhead <daniel@dev.ngo>
2024-03-25 06:45:43 +0000

    Add notes to the top of JS files

--- src/js/lib.js
+++ src/js/lib.js
@@ -1,3 +1,27 @@
+/*
+ * Fuzzy name lists matcher
+ * Copyright (C) 2024 Daniel Fairhead & Contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * -----------------------------------------------------------------------
+ *
+ * This file contains all the algorithm functions for actually doing the fuzzy
+ * matching.
+ *
+ */
+
 export const MATCHVAL = {
   // If it's an *exact* 1:1 match:
   TOTAL: 1000,
